5bdcd85c4d5b74ad78d88f36d553c9b18dd47d4b
saeki <0ec6ky47385587v@gmail.com>
2023-12-06 15:33:44 +0900

    初期値

---
+++ app.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const express = require("express");
+const ejs = require("ejs");
+const app = express();
+const bodyParser = require("body-parser");
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const mysql = require("mysql2");
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "RyoArata0213",
+  database: "express_db",
+});
+
+app.get("/", (req, res) => {
+  con.query("SELECT * FROM board ORDER BY id DESC", (err, results) => {
+    if (err) {
+      console.error('MySQLクエリエラー: ', err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.render("index", { threads: results });
+    }
+  });
+});
+
+app.get("/new-thread", (req, res) => {
+  res.render("new-thread");
+});
+
+app.post("/create-thread", (req, res) => {
+  const threadTitle = req.body.threadTitle;
+  const content = req.body.content;
+  const username = req.body.username;
+
+  con.query(
+    "INSERT INTO threads (title, content, username) VALUES (?, ?, ?)",
+    [threadTitle, content, username],
+    (err, results) => {
+      if (err) {
+        console.error('MySQLクエリエラー: ', err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        res.redirect("/");
+      }
+    }
+  );
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/new', (req, res) => {
+  res.render('new');
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
